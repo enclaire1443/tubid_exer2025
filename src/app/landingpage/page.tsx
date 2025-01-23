d39aff41e06ad0e9223eec1d82a2a9195d804c70
taufan.zamzami <taufan.zamzami@jamiis-macbook.local>
2025-01-23 13:53:56 +0700

    fixed vercel env.local

--- src/app/landingpage/page.tsx
+++ src/app/landingpage/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react' // comment for pushing
 
 export default function Page() {
   const [particles, setParticles] = useState<{ top: string; left: string }[]>([])
